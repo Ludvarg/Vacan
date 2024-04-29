1cb063e8266533d35271f616fd145caf9302187d
Ludvarg <107808954+Ludvarg@users.noreply.github.com>
2024-04-29 18:20:10 +0000

    added Volume 2

--- js/slideshow.js
+++ js/slideshow.js
@@ -17,3 +17,12 @@ function showSlides(n, no) {
     }
     x[slideIndex[no]-1].style.display = "block";  
 }
+
+function scrollToId(id) {
+    let targetElement = document.getElementById("Vacan-Times-" + id);
+
+      targetElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+      });
+}
